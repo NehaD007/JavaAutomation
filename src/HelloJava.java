f586b5e576c941ebf1c8430192c303e3140df11b
NEHA <neha.dehliwale7@gmail.com>
2025-09-14 13:36:37 +0530

    Hello world basic

---
+++ src/HelloJava.java
@@ -0,0 +1,10 @@
+//package com.company;
+public class HelloJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.print("Hi");
+        System.out.print("Hi");
+       // System.out.printf("Hi");
+    }
+}
